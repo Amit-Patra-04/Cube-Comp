557fd1aaed072adf4862add5dbd6757d312e91d3
Amit-Patra-04 <k2amitpatra@gmail.com>
2026-03-18 00:31:20 +0530

    APP.jsx File

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import './App.css'
+import Cubes from './Cubes'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div className="adjust" style={{ height: '600px', position: 'relative' }}>
+  <Cubes 
+    gridSize={5}
+    maxAngle={45}
+    radius={3}
+    borderStyle="2px dashed #B19EEF"
+    faceColor="#1a1a2e"
+    rippleColor="#ff6b6b"
+    rippleSpeed={1.5}
+    autoAnimate
+    rippleOnClick
+  />
+</div>
+    </>
+  )
+}
+
+export default App
